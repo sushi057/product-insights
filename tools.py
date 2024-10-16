f7a13c856f611b3d0bd1e6ea75bbd5e4e3485940
sushi057 <suvash2077@gmail.com>
2024-10-16 21:06:07 +0545

    feat: add hubspot and zedesk api

--- tools.py
+++ tools.py
@@ -1,4 +1,26 @@
+import os
+import base64
+import requests
 from pydantic import BaseModel, Field
+from langchain_core.tools import tool
+
+
+# HubSpot API
+hubspot_api = "https://api.hubapi.com/crm/v3/objects"
+hubspot_headers = {
+    "Authorization": f'Bearer {os.environ["HUBSPOT_BEARER_TOKEN"]}',
+    "Content-Type": "application/json",
+}
+
+# Zendesk API
+zendesk_api = "https://instwisehelp.zendesk.com/api/v2"
+encoded_credentials = base64.b64encode(
+    (f'{os.getenv("ZENDESK_EMAIL")}/token:{os.getenv("ZENDESK_TOKEN")}').encode("utf-8")
+).decode("utf-8")
+zendesk_headers = {
+    "Authorization": f"Basic {encoded_credentials}",
+    "Content-Type": "application/json",
+}
 
 
 class ToCRMAgent(BaseModel):
@@ -38,3 +60,63 @@ class ToChatDataAgent(BaseModel):
 
 
 # CRM Agent Tools
+@tool("hubspot contacts")
+def fetch_hubspot_contacts(hubspot_id: str):
+    """
+    Fetch HubSpot contacts for the given HubSpot ID.
+
+    Args:
+        hubspot_id (str): The HubSpot ID of the customer.
+
+    Returns:
+        str: The response message.
+    """
+    # Fetch HubSpot contacts
+    try:
+        response = requests.get(
+            f"{hubspot_api}/contacts",
+            headers=hubspot_headers,
+        )
+        return response.json()
+    except Exception as e:
+        return f"Error fetching HubSpot contacts: {e}"
+
+
+@tool("hubspot deals")
+def fetch_hubspot_deals(hubspot_id: str):
+    """
+    Fetch HubSpot deals for the given HubSpot ID.
+
+    Args:
+        hubspot_id (str): The HubSpot ID of the customer.
+
+    Returns:
+        str: The response message.
+    """
+    try:
+        # Fetch HubSpot deals
+        response = requests.get(
+            f"{hubspot_api}/deals",
+            headers=hubspot_headers,
+        )
+        return response.json()
+    except Exception as e:
+        return f"Error fetching HubSpot deals: {e}"
+
+
+@tool("zendesk tickets")
+def fetch_zendesk_tickets(zendesk_id: str):
+    """
+    Fetch Zendesk tickets for the given Zendesk ID.
+
+    Args:
+        zendesk_id (str): The Zendesk ID of the customer.
+
+    Returns:
+        str: The response message.
+    """
+    try:
+        response = requests.get(zendesk_api, headers=zendesk_headers)
+        return response.json()
+    except Exception as e:
+        return f"Error fetching Zendesk tickets: {e}"
